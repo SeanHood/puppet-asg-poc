c011f4ce44cb098d54c86df4ec4140f2f827fe6d
Sean Hood <me@seanhood.co.uk>
2020-05-20 13:53:00 +0100

    Start httpd

--- site-modules/profile/manifests/appserver.pp
+++ site-modules/profile/manifests/appserver.pp
@@ -7,6 +7,10 @@ class profile::appserver (
     ensure => 'present'
   }
 
+  service { 'httpd':
+    ensure => running
+  }
+
   file { '/var/www/html/index.html':
     ensure  => 'present',
     content => "<h1>${colour}</h1>"
